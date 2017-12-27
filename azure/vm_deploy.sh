2651e21eef802443d42295c14b45209cb7af2810
Alessandro Greganti <algrega@gmail.com>
2017-12-27 17:48:46 +0100

    Create vm_deploy.sh

---
+++ azure/vm_deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Create a resource group.
+az group create --name TEST-RG --location westeurope
+
+# Create a virtual network.
+az network vnet create --resource-group TEST-RG --name myVnet --subnet-name mySubnet
+
+# Create a public IP address.
+az network public-ip create --resource-group TEST-RG --name myPublicIP
+
+# Create a network security group.
+az network nsg create --resource-group TEST-RG --name myNetworkSecurityGroup
+
+# Create a virtual network card and associate with public IP address and NSG.
+az network nic create \
+  --resource-group TEST-RG \
+  --name myNic \
+  --vnet-name myVnet \
+  --subnet mySubnet \
+  --network-security-group myNetworkSecurityGroup \
+  --public-ip-address myPublicIP
+
+# Create a new virtual machine, this creates SSH keys if not present.
+az vm create --resource-group TEST-RG --name myVM --nics myNic --image CentOS --admin-username algrega --size Standard_B1ms
+
+# Open port 22 to allow SSh traffic to host.
+az vm open-port --port 22 --resource-group TEST-RG --name myVM
+
+# Create a Recovery Services vault
+az backup vault create --resource-group TEST-RG \
+    --name myRecoveryServicesVault \
+    --location westeurope
+
+# Enable backup for Azure VM
+az backup protection enable-for-vm \
+    --resource-group TEST-RG \
+    --vault-name myRecoveryServicesVault \
+    --vm myVM \
+    --policy-name DefaultPolicy
